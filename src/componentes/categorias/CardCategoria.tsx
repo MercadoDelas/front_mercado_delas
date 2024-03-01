e6c60cdda28ec12a2e1d26764e20e72e5c372a2e
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-03-01 10:40:25 -0300

    ajust in CardCategoria

--- src/componentes/categorias/CardCategoria.tsx
+++ src/componentes/categorias/CardCategoria.tsx
@@ -1,35 +1,43 @@
-
-import { Link } from 'react-router-dom'
-import Categoria from '../../models/Categoria'
+import { Link } from "react-router-dom";
+import Categoria from "../../models/Categoria";
 
 interface CardCategoriaProps {
-  categoria: Categoria
+  categoria: Categoria;
 }
 
 function CardCategoria({ categoria }: CardCategoriaProps) {
   return (
     <>
-      <div className=" h-screen max-w-2xl mx-auto mt-24 space-y-20">
-        <div className=" md:w-3/4 mx-auto">
-          <div className="w-96 inline-flex flex-col space-y-2 items-center justify-end flex-1 h-full p-4 bg-violet-500 rounded-xl">
-            <p className="w-full text-2xl font-semibold text-white">{categoria.tipo}</p>
-            <p className="w-full pb-8 text-sm tracking-wide leading-tight text-white">{categoria.descricao}</p>
-            <div className="rounded mr-auto flex items-center justify-center">
-              <div className="opacity-95 border rounded-lg border-white px-4 mr-4 w-20">
-                <Link to={`/editarCategoria/${categoria.id}`} className="m-auto inset-0 text-sm font-medium leading-normal text-center text-white py-2 ">Editar</Link>
-              </div>
-              <div className="opacity-95 border rounded-lg border-white px-4 w-20">
-                <Link to={`/deletarCategoria/${categoria.id}`} className="m-auto inset-0 text-sm font-medium leading-normal text-center text-white py-2">Deletar</Link>
-              </div>
+      <div className="mt-10">
+        <div className="w-96 inline-flex flex-col space-y-2 items-center justify-end flex-1 h-full p-4 bg-violet-500 rounded-xl">
+          <p className="w-full text-2xl font-semibold text-white">
+            {categoria.tipo}
+          </p>
+          <p className="w-full pb-8 text-sm tracking-wide leading-tight text-white">
+            {categoria.descricao}
+          </p>
+          <div className="rounded mr-auto flex items-center justify-center">
+            <div className="opacity-95 border rounded-lg border-white px-4 mr-4w-20">
+              <Link
+                to={`/editarCategoria/${categoria.id}`}
+                className="m-auto inset-0 text-sm font-medium leading-normal text-center text-white py-2 "
+              >
+                Editar
+              </Link>
+            </div>
+            <div className="opacity-95 border rounded-lg border-white px-4 w-20">
+              <Link
+                to={`/deletarCategoria/${categoria.id}`}
+                className="m-auto inset-0 text-sm font-medium leading-normal text-center text-white py-2"
+              >
+                Deletar
+              </Link>
             </div>
           </div>
         </div>
-
-
       </div>
     </>
-
-  )
+  );
 }
 
-export default CardCategoria
+export default CardCategoria;
--- src/componentes/categorias/ListaCategoria.tsx
+++ src/componentes/categorias/ListaCategoria.tsx
@@ -1,13 +1,11 @@
-import  { useContext, useEffect, useState } from 'react';
-import { Dna } from 'react-loader-spinner';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../contexts/AuthContext';
-import { buscar } from '../../services/Service';
-import Categoria from '../../models/Categoria';
-import { toastAlerta } from '../../utils/toastAlerta';
-import CardCategoria from './CardCategoria';
-
-
+import { useContext, useEffect, useState } from "react";
+import { Dna } from "react-loader-spinner";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import { buscar } from "../../services/Service";
+import Categoria from "../../models/Categoria";
+import { toastAlerta } from "../../utils/toastAlerta";
+import CardCategoria from "./CardCategoria";
 
 function ListaCategoria() {
   const [categorias, setCategorias] = useState<Categoria[]>([]);
@@ -19,21 +17,21 @@ function ListaCategoria() {
 
   async function buscarCategorias() {
     try {
-      await buscar('/categorias', setCategorias, {
+      await buscar("/categorias", setCategorias, {
         headers: { Authorization: token },
       });
     } catch (error: any) {
-      if (error.toString().includes('403')) {
-        toastAlerta('O token expirou, favor logar novamente', 'info')
-        handleLogout()
+      if (error.toString().includes("403")) {
+        toastAlerta("O token expirou, favor logar novamente", "info");
+        handleLogout();
       }
     }
   }
 
   useEffect(() => {
-    if (token === '') {
-      toastAlerta('Você precisa estar logado', 'info');
-      navigate('/login');
+    if (token === "") {
+      toastAlerta("Você precisa estar logado", "info");
+      navigate("/login");
     }
   }, [token]);
 
@@ -52,13 +50,13 @@ function ListaCategoria() {
           wrapperClass="dna-wrapper mx-auto"
         />
       )}
-      <div className="flex justify-center w-full my-4">
-        <div className="container flex flex-col">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-           {categorias.map((categoria) => (
-               <>
+      <div className="flex justify-center w-full">
+        <div className="container ">
+          <div className="grid lg:grid-cols-3">
+            {categorias.map((categoria) => (
+              <>
                 <CardCategoria key={categoria.id} categoria={categoria} />
-              </> 
+              </>
             ))}
           </div>
         </div>
