cb19a7ce00e4517ad612aa9662a7d00f75d09fd0
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-03-01 10:43:20 -0300

    add list product in home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,12 @@
-
 import Carousel from "../../componentes/carousel/Carousel";
+import ListaProdutos from "../../componentes/produtos/ListaProduto";
 
 export default function Home() {
   return (
     <div>
       <Carousel />
       {/* <h1>Home</h1> */}
+      <ListaProdutos />
     </div>
   );
 }
